4a8e1769df79308070ad5be7254a550a301df6f9
Derick <klutseyderick85@gmail.com>
2025-10-17 21:39:20 +0000

    Added UUID, Example , status codes to Todos API

--- todo_with_db/app/models.py
+++ todo_with_db/app/models.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from app.database import Base 
+import uuid
+
 
 class Todo(Base):
     __tablename__ = "todos"  
 
-    id = Column(Integer, primary_key=True, index=True)  
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))  
     title = Column(String(255), nullable=False)        
     description = Column(String(255), nullable=True)   
     completed = Column(Boolean, default=False, nullable=False)  
--- todo_with_db/app/schemas.py
+++ todo_with_db/app/schemas.py
@@ -1,19 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from uuid import UUID
 
 #  Define the data structure for creating and reading todos
 
 class TodoBase(BaseModel):
-    title: str
-    description: str  = None
-    completed: bool = False
+    title: str = Field(...,example="Buy goceries")
+    description: str  = Field(..., example="Buy eggs,milk,bread,vegetables from the supermarket")
+    completed: bool = Field(False, example=False)
 
 class TodoCreate(TodoBase):
      pass
 
 
-
 class TodoResponse(TodoBase):
-    id: int
+    id: UUID
     
 
     class Config:
